72b3810aef12f128831e46d99e3b718f8e1a72f2
yeonwoo <ppupy1209@naver.com>
2023-11-09 12:09:17 +0900

    Setter, 생성자 메서드 추가

--- src/main/java/study/board2/domain/Member.java
+++ src/main/java/study/board2/domain/Member.java
@@ -3,6 +3,7 @@ package study.board2.domain;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import javax.persistence.*;
 
@@ -16,6 +17,15 @@ public class Member {
     @Column(name = "member_id", nullable = false, updatable = false, unique = true)
     private Long id;
 
+    @Setter
     @Column(nullable = false, unique = true)
     private String name; // 멤버 닉네임
+
+    private Member(String name) {
+        this.name = name;
+    }
+
+    public static Member of(String name) {
+        return new Member(name);
+    }
 }
