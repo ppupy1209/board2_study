c4a6f46de04ca6bebaff2ed19217b21a835d66bf
yeonwoo <ppupy1209@naver.com>
2023-11-10 18:30:08 +0900

    UriCreator 구현

---
+++ src/main/java/study/board2/utils/UriCreator.java
@@ -0,0 +1,16 @@
+package study.board2.utils;
+
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.net.URI;
+
+public class UriCreator {
+
+    public static URI createUri(String defaultUrl, Long resourceId) {
+        return UriComponentsBuilder
+                .newInstance()
+                .path(defaultUrl + "/{resource-id}")
+                .buildAndExpand(resourceId)
+                .toUri();
+    }
+}
