43104dad17bdf2e287999c3e07ea1d16d1d49f84
yeonwoo <ppupy1209@naver.com>
2023-11-11 13:53:45 +0900

    초기 데이터 구성

---
+++ src/main/java/study/board2/utils/DataInitializer.java
@@ -0,0 +1,49 @@
+package study.board2.utils;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+import study.board2.domain.Member;
+import study.board2.domain.Question;
+import study.board2.domain.Tag;
+import study.board2.repository.MemberRepository;
+import study.board2.repository.TagRepository;
+import study.board2.service.QuestionService;
+
+import javax.annotation.PostConstruct;
+import java.util.List;
+
+@RequiredArgsConstructor
+@Component
+public class DataInitializer {
+
+    private final MemberRepository memberRepository;
+    private final TagRepository tagRepository;
+
+    private final QuestionService questionService;
+
+    @PostConstruct
+    public void init() {
+        Member member1 = Member.of("kim");
+        Member member2 = Member.of("lee");
+        Member member3 = Member.of("park");
+        Member member4 = Member.of("choi");
+        Member member5 = Member.of("jung");
+
+        memberRepository.saveAll(List.of(member1,member2,member3,member4,member5));
+
+
+        Tag tag1 = Tag.of("spring");
+        Tag tag2 = Tag.of("java");
+        Tag tag3 = Tag.of("python");
+        Tag tag4 = Tag.of("kotlin");
+        Tag tag5 = Tag.of("c++");
+
+        tagRepository.saveAll(List.of(tag1, tag2, tag3, tag4, tag5));
+
+        questionService.saveQuestion(1L, Question.of("제목1","내용1"),"spring,java");
+        questionService.saveQuestion(2L, Question.of("제목2","내용2"),"java,python");
+        questionService.saveQuestion(3L, Question.of("제목3","내용3"),"spring");
+        questionService.saveQuestion(4L, Question.of("제목4","내용4"),"java,kotlin");
+        questionService.saveQuestion(5L, Question.of("제목5","내용5"),"spring,c++");
+    }
+}
