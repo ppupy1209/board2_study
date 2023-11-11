c52acf606e267b24e157cf8d85ee7fe5506f8414
yeonwoo <ppupy1209@naver.com>
2023-11-11 13:50:27 +0900

    TagService 테스트 작성

---
+++ src/test/java/study/board2/service/TagServiceTest.java
@@ -0,0 +1,43 @@
+package study.board2.service;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+import study.board2.domain.Tag;
+import study.board2.repository.TagRepository;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.*;
+
+@Transactional
+@SpringBootTest
+class TagServiceTest {
+
+
+    @Autowired
+    private TagService tagService;
+
+    @Autowired
+    private TagRepository tagRepository;
+
+
+    @DisplayName("태그를 등록한다.")
+    @Test
+    void saveTag() {
+        // given
+        Tag tag = Tag.of("spring");
+
+        // when
+        tagService.saveTag(tag);
+        List<Tag> tags = tagRepository.findAll();
+
+        // then
+        assertThat(tags).hasSize(1)
+                .extracting("name")
+                .containsExactlyInAnyOrder("spring");
+    }
+
+}
