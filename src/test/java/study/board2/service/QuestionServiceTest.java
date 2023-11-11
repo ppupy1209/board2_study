64b313c6b5eed5abd39d9b4733a1de87422cb615
yeonwoo <ppupy1209@naver.com>
2023-11-11 13:50:09 +0900

    QuestionService 테스트 작성

---
+++ src/test/java/study/board2/service/QuestionServiceTest.java
@@ -0,0 +1,44 @@
+package study.board2.service;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+import study.board2.domain.Member;
+import study.board2.domain.Question;
+import study.board2.repository.MemberRepository;
+import study.board2.repository.QuestionRepository;
+
+import static org.assertj.core.api.Assertions.*;
+
+@Transactional
+@SpringBootTest
+class QuestionServiceTest {
+
+
+    @Autowired
+    private QuestionService questionService;
+
+    @Autowired
+    private QuestionRepository questionRepository;
+
+    @Autowired
+    private MemberRepository memberRepository;
+
+
+    @DisplayName("질문을 등록한다.")
+    @Test
+    void saveQuestion() {
+        // given
+        Member member = memberRepository.save(Member.of("testName"));
+        Question request = Question.of("제목","내용");
+        String tags = "spring,java";
+
+        // when
+        Long questionId = questionService.saveQuestion(member.getId(), request, tags);
+
+        // then
+        assertThat(questionId).isNotNull();
+    }
+}
