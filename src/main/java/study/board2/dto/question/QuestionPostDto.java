2d3d7044449e4e9f0138b00709cec7710e4d9dd7
yeonwoo <ppupy1209@naver.com>
2023-11-11 13:52:40 +0900

    QuestionPostDto 작성

---
+++ src/main/java/study/board2/dto/question/QuestionPostDto.java
@@ -0,0 +1,23 @@
+package study.board2.dto.question;
+
+import lombok.Getter;
+import study.board2.domain.Question;
+
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
+
+@Getter
+public class QuestionPostDto {
+
+    private Long memberId;
+    @NotBlank(message = "제목은 필수입니다.")
+    private String title;
+    @NotBlank(message = "내용은 필수입니다.")
+    private String content;
+
+    private String tags;
+
+    public Question toEntity() {
+        return Question.of(title,content);
+    }
+}
