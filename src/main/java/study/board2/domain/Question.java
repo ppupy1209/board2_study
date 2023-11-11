d48fe157449754127a3369e2703feb2cddf24814
yeonwoo <ppupy1209@naver.com>
2023-11-11 13:51:46 +0900

    생성자 추가, Member 연관관계 매핑

--- src/main/java/study/board2/domain/Question.java
+++ src/main/java/study/board2/domain/Question.java
@@ -3,8 +3,11 @@ package study.board2.domain;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import javax.persistence.*;
+
+import static javax.persistence.FetchType.*;
 
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
@@ -22,5 +25,23 @@ public class Question {
     @Column(name = "content", nullable = false)
     private String content; // 질문 내용
 
+    @Setter
+    @ManyToOne(fetch = LAZY)
+    @JoinColumn(name = "member_id")
+    private Member member;
 
+
+    private Question(String title, String content, Member member) {
+        this.title = title;
+        this.content = content;
+        this.member = member;
+    }
+
+    public static Question of(String title, String content, Member member) {
+        return new Question(title, content, member);
+    }
+
+    public static Question of(String title, String content) {
+        return Question.of(title,content,null);
+    }
 }
