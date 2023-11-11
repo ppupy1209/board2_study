367e82c9a7788ae58a3c497f3e808b165a3b6608
yeonwoo <ppupy1209@naver.com>
2023-11-11 13:53:01 +0900

    예외 코드 추가

--- src/main/java/study/board2/exception/ExceptionCode.java
+++ src/main/java/study/board2/exception/ExceptionCode.java
@@ -4,8 +4,11 @@ import lombok.Getter;
 
 public enum ExceptionCode {
 
-    MEMBER_NOT_FOUND(404, "Member Not Found");
+    MEMBER_NOT_FOUND(404, "Member Not Found"),
+    TAG_NOT_FOUND(404,"Tag Not Found"),
 
+
+    ALREADY_MEMBER_NAME_EXISTS(409,"Already Member_Name Exists");
     @Getter
     int status;
 
