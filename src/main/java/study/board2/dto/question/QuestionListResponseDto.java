08a0b995c847aae74f0ca5e9bb366a27b3fa0421
yeonwoo <ppupy1209@naver.com>
2023-11-14 16:44:45 +0900

    Question 리스트 Response DTO

--- src/main/java/study/board2/dto/question/QuestionListResponseDto.java
+++ src/main/java/study/board2/dto/question/QuestionListResponseDto.java
@@ -1,6 +1,27 @@
 package study.board2.dto.question;
 
+import lombok.Getter;
+import study.board2.domain.Question;
+
+
+@Getter
 public class QuestionListResponseDto {
 
+    private String writer;
+    private String title;
+    private Integer answerCnt;
 
+    public QuestionListResponseDto(String writer, String title, Integer answerCnt) {
+        this.writer = writer;
+        this.title = title;
+        this.answerCnt = answerCnt;
+    }
+
+    public static QuestionListResponseDto of(Question question) {
+        return new QuestionListResponseDto(
+                question.getMember().getName(),
+                question.getTitle(),
+                question.getAnswers().size()
+        );
+    }
 }
